36acbc93ee4ed55bbdec1b908f4945396d0934b5
kavyasudha <kavyakrishna97@gmail.com>
2019-11-21 10:00:55 +0530

    Update discord.py

--- discord.py
+++ discord.py
@@ -4,7 +4,7 @@ from googlesearch import search
 TOKEN = input("Enter the token of bot: ")
 
 client = discord.Client()
-recent_history = list()
+
 
 def query_results(str):
     query = list()
@@ -17,16 +17,21 @@ async def on_message(message):
     print(message.content)
     if message.content.lower().startswith('hi'):
         await message.channel.send('hey')
-    if message.content.lower().startswith('!google '):
+    elif message.content.lower().startswith('!google '):
         query = (message.content.lower()).split("!google ", 1)
+        if len(query) == 1:
+            await message.channel.send("Please enter valid search item")
         with open("query_kavya.txt", "a") as write_file:
             write_file.write(query[1].lower() + "\n")
         result = query_results(query[1])
         for j in result:
             await message.channel.send(j)
 
-    if message.content.lower().startswith('!recent '):
+    elif message.content.lower().startswith('!recent '):
         query = (message.content.lower()).split("!recent ", 1)
+        recent_history = list()
+        if len(query) == 1:
+            await message.channel.send("Please enter valid search item")
         search_word = query[1].lower()
         str1 = ""
         with open('query_kavya.txt') as file:
@@ -39,9 +44,15 @@ async def on_message(message):
             await message.channel.send("No recent searches found for " + search_word)
         else:
             history = list(set(recent_history))
+            print(history)
             await message.channel.send("Recent searches for " + search_word + " are as follows: \n")
             for i in history:
                 await message.channel.send(i)
+            history.clear()
+
+    elif message.content.lower().startswith('!'):
+        await message.channel.send("Please enter valid cases")
+
 
 @client.event
 async def on_ready():
